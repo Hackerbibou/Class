6443b18d63a5831154b098905095c4a43df34b83
haris <Haris199x@gmail.com>
2024-10-16 22:35:39 -0500

    push

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://olrfgwsbgyajiicxsnhz.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
+console.log(supabaseKey)
+console.log(supabaseUrl)
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
