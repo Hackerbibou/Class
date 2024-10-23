ae2579a29b4467878737a95a1d56d01336b95fbb
Habib <79129877+Hackerbibou@users.noreply.github.com>
2024-10-23 17:00:01 +0000

    checkout

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://olrfgwsbgyajiicxsnhz.supabase.co'
+const supabaseUrl = process.env.SUPABASE_URL
 const supabaseKey = process.env.SUPABASE_KEY
-console.log(supabaseKey)
-console.log(supabaseUrl)
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
