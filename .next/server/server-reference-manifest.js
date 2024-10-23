2ddd266799a15e3d1e3c6f6f82c9c50abe7aae69
Habib <79129877+Hackerbibou@users.noreply.github.com>
2024-10-23 20:44:50 +0000

    Pending changes exported from your codespace

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"s61vS3JPwO3oDFVNLK7hCz03CgZLXE5crNHWVm+Qi38=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"GjCf+vimKw59R4tJDgTbXagH8urMTLNOLyxYUP7CTnQ=\"\n}"
