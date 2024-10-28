c5b7e29eef722c9059b82de26eabb874c97bce4d
Abiboulayed <79129877+Hackerbibou@users.noreply.github.com>
2024-10-28 13:26:10 -0400

    f

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"kFUzm+7gp4vEvelQ23qdmtKtKjjOdNTxb41n/mYka6s=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
